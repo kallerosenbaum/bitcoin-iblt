ed0a46c464a571fb99e2cfdb50e53c6d35582fca
Kalle Rosenbaum <kalle@rosenbaum.se>
2014-12-03 18:44:43 +0100

    Added similar test as Rusty Russell

--- src/test/java/se/rosenbaum/bitcoiniblt/DiffCountVSFailureProbabilityTestManual.java
+++ src/test/java/se/rosenbaum/bitcoiniblt/DiffCountVSFailureProbabilityTestManual.java
@@ -10,6 +10,27 @@ import java.io.IOException;
 public class DiffCountVSFailureProbabilityTestManual extends BlockStatsClientCoderTest {
 
     @Test
+    public void testRustyRussell() throws IOException {
+        TestConfig config = new TestConfig(0, 0, 0, 4, 8, 8, 4, 52428, true);
+
+        DiffCountVSFailureProbabilityPrinter printer = new DiffCountVSFailureProbabilityPrinter(tempDirectory);
+
+        int[] tests = new int[]{700, 800, 900, 1000, 1100, 1200, 1300};
+        for (int i = 0; i < tests.length; i++) {
+            int diffs = tests[i];
+            int halfDiffs = diffs / 2;
+            config.setTxCount(halfDiffs);
+            config.setExtraTxCount(halfDiffs);
+            config.setAbsentTxCount(halfDiffs);
+
+//            ResultStats result = testFailureProbability(printer, config, 100);
+            ResultStats result = testFailureProbability(printer, config, 1000);
+            printer.addResult(config, result);
+        }
+        printer.finish();
+    }
+
+    @Test
     public void testDiffCountVSFailureProbability() throws IOException {
         TestConfig config = new TestConfig(0, 0, 0, 3, 8, 64, 4, 3000, true);
 
